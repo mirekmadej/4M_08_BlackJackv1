5823f33f7eee284941e71a5e66a5290a5f43adcc
Mirek Madej <m.madej@zsp3zamosc.pl>
2023-10-26 08:54:52 +0200

    dodano obsługę talii kart

--- 4M_08_BlackJackv1/MainPage.xaml.cs
+++ 4M_08_BlackJackv1/MainPage.xaml.cs
@@ -2,22 +2,46 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+        private string[] talia = new string[52];
+        private int pK, pKas, pG, pGas;
         public MainPage()
         {
             InitializeComponent();
+            generujTalie();
+        }
+        private void generujTalie()
+        {           
+            string[] kolory = { "Pik", "Kier", "Karo", "Trefl" };
+            int poz = 0;
+            for (int i = 0; i < kolory.Length; i++)
+            {
+                talia[poz++] = "A " + kolory[i];
+                for (int j = 2; j <= 10; j++)
+                    talia[poz++] = j.ToString() + " " + kolory[i];
+                talia[poz++] = "W " + kolory[i];
+                talia[poz++] = "D " + kolory[i];
+                talia[poz++] = "K " + kolory[i];
+            }
+            pK = pKas = pG = pGas = 0;
         }
 
-        private void btnNowaClicked(object sender, EventArgs e)
+        private void btnNowaGraClicked(object sender, EventArgs e)
         {
-            btnGraj.IsVisible = true;
-            btnNowa.IsVisible = false;
+            generujTalie();
+            /*    1. ukryć ten button
+             *    2. pokazać przycisk dobierz i pass
+             *    3. wylosować karty dla krupiera, obliczyć wyniki
+             *    4. wylosować karty dla gracza, obliczyć wuniki 
+             *    5. wyswietlić wyniki gracza
+             */  
         }
-        private void btnGrajClicked(object sender, EventArgs e)
+        private void btnDobierzClicked(object sender, EventArgs e)
         {
-            btnGraj.IsVisible = false;
-            btnNowa.IsVisible = true;
+            
+        }
+        private void btnPassClicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
